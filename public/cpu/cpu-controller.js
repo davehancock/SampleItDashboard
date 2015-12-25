01ad4322d6eb78cf5afa97824497069d3f27a88b
Dave <daves125125@gmail.com>
2015-12-25 00:24:11 +0000

    Dockerization

--- public/cpu/cpu-controller.js
+++ public/cpu/cpu-controller.js
@@ -1,32 +1,41 @@
 app.controller('CpuCtrl', function ($scope, $timeout, CpuService, HostService) {
 
+    var sampleHost = "UnknownHost";
+
     HostService.retrieveHosts().success(function (data) {
         $scope.hosts = data;
+        sampleHost = data[0];
+
+        createOverlayChart();
     });
+
+    $scope.activeHost = function (tab) {
+        sampleHost = tab;
+        //refreshOverlayChart();
+    };
 
     var cpuMetricPoller = function () {
         refreshOverlayChart();
         $timeout(cpuMetricPoller, 5000);
     };
 
+    // This needs to be called on the promise for hosts
     var createOverlayChart = function () {
 
-        CpuService.retrieveOverlayCpuMetrics().success(function (data) {
-            $scope.labels = CpuService.populateLabels();
+        CpuService.retrieveCpuMetrics(sampleHost).success(function (data) {
+            $scope.labels = CpuService.populateLabels(data);
             $scope.series = CpuService.populateSeries(data.length);
             $scope.cpuDataSeries = data;
+
+            cpuMetricPoller();
         });
     };
 
     var refreshOverlayChart = function () {
 
-        CpuService.retrieveOverlayCpuMetrics().success(function (data) {
+        CpuService.retrieveCpuMetrics(sampleHost).success(function (data) {
             $scope.cpuDataSeries = data;
         });
     };
-
-    // Kick off the poller
-    createOverlayChart();
-    cpuMetricPoller();
 
 });
--- public/cpu/cpu-service.js
+++ public/cpu/cpu-service.js
@@ -1,18 +1,20 @@
 app.service('CpuService', function ($http, hostname, port) {
 
-    this.retrieveCpuMetrics = function () {
-        return $http.get(hostname + ':' + port + '/cpus');
+    this.retrieveCpuMetrics = function (sampleHost) {
+        return $http.get(hostname + ':' + port + '/cpu/samples/' + sampleHost + '/');
     };
 
-    this.retrieveOverlayCpuMetrics = function () {
-        return $http.get(hostname + ':' + port + '/cpu/overlay/');
-    };
+    this.populateLabels = function (data) {
 
-    this.populateLabels = function () {
+        var numberOfItems = 0;
+
+        if (data.length > 0) {
+            numberOfItems = data[0].length;
+        }
 
         var labelArr = [];
 
-        for (var i = 0; i < 30; i++) {
+        for (var i = 0; i < numberOfItems; i++) {
             labelArr.push(i + 1);
         }
 
